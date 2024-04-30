cf9e8555632b348d81bc005982ad3b68729d8e29
KoushikPanda18 <koushikpandanew018@gamil.com>
2024-05-01 00:09:51 +0530

    added basic bodyParser

--- src/config/serverconfig.js
+++ src/config/serverconfig.js
@@ -1,4 +1,4 @@
-//in this js file packages require stufs are done
+//in this js file packages require stufs are done....because the main server index.js file is responsible for booting the most important piece of code,so we don't want to make this file clumsy with environt variable details
 
 
 const dotenv=require("dotenv");
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express=require('express');
+const bodeParser=require('body-parser');
 
 const {PORT}=require("./config/serverconfig");
+const bodyParser = require('body-parser');
 
 const appServer=async()=>{
     //create the express object
     const app=express();
+
+    //setting up middlewares for bodyParser
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}))
+
     app.listen(PORT,()=>{
         console.log("server statred at port",PORT);
         //console.log(process.env.PORT);
